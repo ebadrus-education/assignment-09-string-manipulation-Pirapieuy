86ed255a6489bd80c218dadf9a8e655caefe5e0c
pirapiieuyy <nuralfanrafi@gmail.com>
2025-11-05 21:54:16 +0700

    Add main.cpp to demonstrate string length and size

---
+++ teks.length_size /main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string teks = "Belajar C++";
+    cout << "Teks: " << teks << endl;
+    cout << "Panjang menggunakan length(): " << teks.length() << endl;
+    cout << "Panjang menggunakan size(): " << teks.size() << endl;
+}
